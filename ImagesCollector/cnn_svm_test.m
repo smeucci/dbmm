b563a3e546cbabbc5740e95b441dd0d287c081e2
Saverio <s.meucci91@gmail.com>
2016-03-03 12:52:30 +0100

    updated 5.3

--- ImagesCollector/cnn_svm_test.m
+++ ImagesCollector/cnn_svm_test.m
@@ -24,25 +24,35 @@ feat_layer = 36;
 
 % Load pre trained net
 if ~exist('net','var')
-    fprintf('Loading ConvNet\n');
+    fprintf('Loading ConvNet\n\n');
     net = load(net_path);
     load([data_path, 'data/dataset_lab_CNN_correct.mat']);
 else
-    fprintf('ConvNet already loaded\n');
+    fprintf('ConvNet already loaded\n\n');
 end
 
 load([data_path, 'data/model.mat']);
 
-index = 1;
+%connect to database
+conn = database('dataset', 'root', 'pwd', 'Vendor', 'MySQL', 'Server', 'localhost');
+
+
 for i = 1:1%size(dataset, 1)
     
+    index = 1;
     identity = dataset(i,:);
     identity_path = [data_path, 'img/', identity{2}, '_', identity{1}, '/'];
+    fprintf('Images classification of identity %s - %s\n', identity{2}, identity{1});
     
     data_test = [];
-    for j = 50:100%size(identity{3}, 2)
+    tot = 0;
+    max_size = 100;%size(identity{3}, 2);
+    
+    reverseStr = '';
+    
+    for j = 1:max_size
+        
         % take the image and crop it using the face detection coordinates.
-        disp(j);
         im_data = identity{3}(j);
         im_data.image = strtrim(strrep(im_data.image, 'Premature end of JPEG file', ''));
         identity{3}(j).image = im_data.image;
@@ -56,10 +66,10 @@ for i = 1:1%size(dataset, 1)
         im_ = imresize(im_, net.normalization.imageSize(1:2)) ;
         im_ = bsxfun(@minus,im_,net.normalization.averageImage) ;
         res = vl_simplenn(net, im_);
-        feature = squeeze(res(feat_layer).x);
+        feature = squeeze(res(feat_layer).x)';
         
         % predict the class of the image using a pre-computed svm model.
-        predicted_label = lib.libsvm.svmpredict(i, feature', model);
+        [output, predicted_label] = evalc('lib.libsvm.svmpredict(i, double(feature), model)');
         
         % save result as field struct.
         
@@ -69,20 +79,28 @@ for i = 1:1%size(dataset, 1)
                                 num2str(im_data.box.right), ',', num2str(im_data.box.bottom)];
         if i == predicted_label
             data_test(index).predicted = 1;
+            tot = tot + 1;
         else
             data_test(index).predicted = 0;
         end
       
         index = index + 1;
+        
+        %print progress
+        msg = sprintf('#Progress identity %s - %s: %d', identity{2}, identity{1}, ceil(100*(j/max_size)));
+        fprintf([reverseStr, msg]);
+        reverseStr = repmat(sprintf('\b'), 1, length(msg));
     end
+    fprintf('\n');
     
     %save results to db, 
     data_to_insert = struct2table(data_test);
-    %connect to db
+    
+    %fastinsert(conn, 'images', {'image', 'identity', 'box', 'predicted'}, data_to_insert); 
+    %update(conn, 'identities', {'num_images'}, tot, ['where label = "', identity{2}, '"']);
     
     fprintf(' - Identity: %s - %s, Elapsed time: %.2f s\n', identity{1}, identity{2}, etime(clock, start_time));
     
 end
-
 
 fprintf('\n - END- Elapsed time: %.2f s\n', etime(clock, start_time));
