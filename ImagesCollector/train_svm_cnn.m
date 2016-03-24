1fc74a3de7334c1a7476e835da219009c0a524c3
Saverio <s.meucci91@gmail.com>
2016-03-24 14:04:42 +0100

    7.2

--- ImagesCollector/train_svm_cnn.m
+++ ImagesCollector/train_svm_cnn.m
@@ -6,7 +6,7 @@ function train_svm_cnn(DATA_PATH, start_idx, end_idx, start_time, config)
     %warning off MATLAB:colon:nonIntegerIndex;
     warning off;
     
-    addpath(genpath('/home/saverio/Ingegneria/Visual And Multimedia Recognition/Elaborato/matconvnet'));
+    addpath(genpath(config.MATCONVENT_PATH));
     vl_setupnn;
 
     %load dataset 
